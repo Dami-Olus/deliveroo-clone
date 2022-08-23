061a00dc14380c72f3ccef01fa0f5281ea8b4451
Damola Olusakin <olusakinadedamola@gmail.com>
2022-08-23 15:06:31 +0100

    added Preparing order screen and delivery screen

--- screens/BasketScreen.js
+++ screens/BasketScreen.js
@@ -121,7 +121,7 @@ const BasketScreen = () => {
               <Currency quantity={basketTotal + 5.99} currency="GBP" />
             </Text>
           </View>
-          <TouchableOpacity className="rounded-lg bg-[#00CCBB] p-4">
+          <TouchableOpacity onPress={()=> navigation.navigate("PreparingOrder")} className="rounded-lg bg-[#00CCBB] p-4">
             <Text className="text-center text-white text-lg font-bold">Place Order</Text>
           </TouchableOpacity>
         </View>
---
+++ screens/DeliveryScreen.js
@@ -0,0 +1,11 @@
+import { View, Text } from 'react-native'
+
+const DeliveryScreen = () => {
+  return (
+    <View>
+      <Text>Delivery Screen</Text>
+    </View>
+  )
+}
+
+export default DeliveryScreen
